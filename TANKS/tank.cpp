784156a3581c5af619fb94c0e1a38239a5f4414d
ZhekLu <justletmego@list.ru>
2021-05-28 00:01:58 +0300

    Fix Move&Rotate functions

--- TANKS/tank.cpp
+++ TANKS/tank.cpp
@@ -1,16 +1,19 @@
 #include "tank.h"
 
 
-Tank::Tank(int x, int y, QGraphicsScene *scene, QWidget *parent) : QWidget(parent)
+Tank::Tank(int x, int y, QGraphicsScene *scene, int rotation, QWidget *parent) : QWidget(parent)
 {
     bodyLen = CELL * 3;
+    this->rotation = rotation;
+    Rotate(rotation);
 //    body
     body = new QGraphicsRectItem(0, 0, bodyLen, bodyLen);
     body->setPen(QPen(Qt::black, 4, Qt::SolidLine, Qt::FlatCap, Qt::RoundJoin));
     body->setBrush(Qt::gray);
     body->setPos(x, y);
 //    child cannon
-    cannon = new QGraphicsRectItem(CELL, -CELL, CELL * 1, CELL * 2, body);
+    cannon = new QGraphicsRectItem(0, 0, CELL * 1, CELL * 2, body);
+    cannon->setPos(CELL, 2 * CELL);
     cannon->setPen(QPen(Qt::black, 2, Qt::SolidLine, Qt::FlatCap, Qt::RoundJoin));
     cannon->setBrush(Qt::gray);
 //    bullet
@@ -20,4 +23,58 @@ Tank::Tank(int x, int y, QGraphicsScene *scene, QWidget *parent) : QWidget(paren
     bullet->hide();
 
     scene->addItem(body);
+}
+
+void Tank::Move(int step)
+{
+    QPointF currPos = body->pos();
+    switch (rotation)
+    {
+    case Rotation::LEFT:
+        currPos.rx() -= step;
+        break;
+    case Rotation::UP:
+        currPos.ry() -= step;
+        break;
+    case Rotation::RIGHT:
+        currPos.rx() += step;
+
+        break;
+    case Rotation::DOWN:
+        currPos.ry() += step;
+        break;
+    }
+    body->setPos(currPos);
+}
+
+void Tank::Rotate(int rot)
+{
+    switch (rot)
+    {
+    case Rotation::LEFT:
+//        cannon->setRotation(90);
+        cannon->setRect(0, 0, CELL * 2, CELL * 1);
+        cannon->setPos(-1 * CELL, 1 * CELL);
+        rotation = Rotation::LEFT;
+        break;
+    case Rotation::UP:
+//        cannon->setRotation(180);
+        cannon->setRect(0, 0, CELL * 1, CELL * 2);
+        cannon->setPos(1 * CELL, -1 * CELL);
+        rotation = Rotation::UP;
+        break;
+    case Rotation::RIGHT:
+//        cannon->setRotation(270);
+        cannon->setRect(0, 0, CELL * 2, CELL * 1);
+        cannon->setPos(2 * CELL, 1 * CELL);
+        rotation = Rotation::RIGHT;
+
+        break;
+    case Rotation::DOWN:
+//        cannon->setRotation(0);
+        cannon->setRect(0, 0, CELL * 1, CELL * 2);
+        cannon->setPos(1 * CELL, 2 * CELL);
+        rotation = Rotation::DOWN;
+        break;
+    }
 }
--- TANKS/tank.h
+++ TANKS/tank.h
@@ -1,22 +1,34 @@
 #ifndef TANK_H
 #define TANK_H
 
+#include<QDebug>
+
 #include<QWidget>
 #include<QGraphicsView>
 #include<QGraphicsScene>
 #include<QGraphicsRectItem>
 #include<QGraphicsEllipseItem>
+#include<QPointF>
 #include "definecells.h"
 class Tank : public QWidget
 {
     Q_OBJECT
 public:
-    Tank(int x, int y, QGraphicsScene *scene, QWidget* parent = nullptr);
+    Tank(int x, int y, QGraphicsScene *scene, int rotation = Rotation::STOP, QWidget* parent = nullptr);
+    enum Rotation
+    {
+        LEFT, UP, RIGHT, DOWN, STOP
+    };
+    int rotation;
+    void Move(int step = CELL);
+    void Rotate(int rot);
+//    QPointF Pos();
 private:
     QGraphicsRectItem* cannon; //pushka
     QGraphicsRectItem* body;
     QGraphicsEllipseItem* bullet; // shot
     int bodyLen;
+
 };
 
 #endif // TANK_H
