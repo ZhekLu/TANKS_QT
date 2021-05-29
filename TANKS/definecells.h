4c6bd25127f2138a28962ab3ebe6cea460651613
ZhekLu <justletmego@list.ru>
2021-05-29 22:05:04 +0300

    Some variables define

--- TANKS/definecells.h
+++ TANKS/definecells.h
@@ -2,7 +2,8 @@
 #define DEFINECELLS_H
 
 #define CELL 20
-#define WIDTH CELL * 15
+#define WIDTH CELL * 15 * 2     // 600
+#define HEIGHT CELL * 14 * 2    // 560
 enum Rotation
 {
     LEFT, UP, RIGHT, DOWN, STOP
